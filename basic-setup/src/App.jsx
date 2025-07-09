e77fb6ca866fc819c8bc79d9f3fa6e35100f1534
Pranavraj09 <pranavraj0909@gmail.com>
2025-07-09 22:00:27 +0530

    done

--- basic-setup/src/App.jsx
+++ basic-setup/src/App.jsx
@@ -4,6 +4,14 @@ import './App.css'
 import Mode from './mode/Mode';
 
 function App() {
+  // State to manage the theme
+  // 'light' for light mode, 'dark' for dark mode
+  // The initial theme is set to 'light'
+  // The toggleTheme function switches between light and dark modes
+  // The appStyle object dynamically changes styles based on the current theme
+  // The appStyle object is applied to the main container to reflect the current theme
+  // The component renders a heading and the Mode component, passing the current theme and toggle function
+  // for use theme and settheme
   const[theme, setTheme] = useState('light');
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
