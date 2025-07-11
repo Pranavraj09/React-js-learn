35b7c97af23d41debec72bb0ac1e9ff067a97ce5
Pranavraj09 <pranavraj0909@gmail.com>
2025-07-11 21:35:04 +0530

    done

--- basic-setup/src/mode/Mode.jsx
+++ basic-setup/src/mode/Mode.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 export default function Mode({ modeTheme, modetoggleTheme }) {
   return (
-    <div
-      style={{
+    <div style={{
         padding: '10px 20px',
         backgroundColor: modeTheme === 'light' ? '#eee' : '#333',
         color: modeTheme === 'light' ? '#000' : '#fff',
         display: 'flex',
         justifyContent: 'space-between',
-      }}
-    >
-      <span>My App</span>
+      }}>
+      <div>My App</div>
       <button className='btn btn-info'onClick={modetoggleTheme}>
         Switch to {modeTheme === 'light' ? 'Dark' : 'Light'} Mode
       </button>
